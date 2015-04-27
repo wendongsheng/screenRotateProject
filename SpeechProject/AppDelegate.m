81e499d33b504ac6eae78fa9c76315a0e1bf799e
wendongsheng <18637117435@163.com>
2015-04-27 10:42:48 +0800

    add IQKeyboardManager

--- SpeechProject/AppDelegate.m
+++ SpeechProject/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "RootViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.window.rootViewController = [RootViewController new];
+    
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
